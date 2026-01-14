63124b9433c32d9c63e2e689a9fd8b940e3dc979
JasperRosales <23-19300@g.batstate-u.edu.ph>
2026-01-14 23:56:30 +0800

    added something

--- cmd/main.go
+++ cmd/main.go
@@ -5,27 +5,48 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/joho/godotenv"
 )
 
-func Hello(w http.ResponseWriter, r *http.Request) {
+type api struct{
+    addr string
+}
+
+
+func (api *api) Hello(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello, World!\n"))
 }
 
+
 func main() {
-    godotenv.Load()
-    
-    http.HandleFunc("/", Hello)
+	godotenv.Load()
 
     port := os.Getenv("PORT")
-    if port == "" {
-        log.Fatal("PORT environment variable not set") 
+	if port == "" {
+		log.Fatal("PORT environment variable not set")
+	}
+
+    api := &api{
+        addr: fmt.Sprintf(":%s", port),
     }
 
-    addr := fmt.Sprintf(":%s", port)
-    log.Println("Server starting on", addr)
+    mux := http.NewServeMux()
 
-    err := http.ListenAndServe(addr, nil)
-    log.Fatal(err) 
+	s := &http.Server{
+		Addr:           api.addr,
+		Handler:        mux,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+
+	mux.HandleFunc("GET /hello", api.Hello)
+
+	
+	log.Println("Server starting on", s.Addr)
+
+	err := http.ListenAndServe(s.Addr, nil)
+	log.Fatal(err)
 }
